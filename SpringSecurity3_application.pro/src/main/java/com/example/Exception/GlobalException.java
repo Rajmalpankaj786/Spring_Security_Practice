fda18e02c7f0eee0f92597b9be7ddea7430f58ea
rajmalpankaj786 <rajmalpankaj786@gmail.com>
2024-04-20 11:34:03 +0530

    authentaction using application.property file

---
+++ SpringSecurity3_application.pro/src/main/java/com/example/Exception/GlobalException.java
@@ -0,0 +1,24 @@
+package com.example.Exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalException {
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<MyError> notFoundException(NotFoundException NPE , WebRequest req){
+		MyError err = new MyError();
+		err.setTimestamp(LocalDateTime.now());
+		err.setDetails(req.getDescription(false));
+		err.setMsg(NPE.getMessage());
+		
+		return new ResponseEntity<MyError>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+}
---
+++ SpringSecurity3_application.pro/src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+	@Bean
+	public SecurityFilterChain EmployeeMethods(HttpSecurity http) throws Exception{
+		http.authorizeHttpRequests(
+				auth -> auth.requestMatchers(HttpMethod.POST, "/employees/post").permitAll().anyRequest().authenticated())
+				.csrf(c -> c.disable())
+				.httpBasic(Customizer.withDefaults())
+				.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
+}
